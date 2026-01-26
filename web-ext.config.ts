25fe5c9128c66917ab75f8871f8863e013d9b40a
shuijiaowang <11915233+liyuann@user.noreply.gitee.com>
2026-01-27 01:26:30 +0800

    配置

---
+++ web-ext.config.ts
@@ -0,0 +1,9 @@
+import { defineWebExtConfig } from 'wxt';
+import { resolve } from 'node:path';
+export default defineWebExtConfig({
+    startUrls: ["https://www.bilibili.com/"],
+    // startUrls: ["https://www.doubao.com/chat/coding","https://chat.deepseek.com/","https://baidu.com/"],
+    // On Windows, the path must be absolute
+    chromiumProfile: resolve('.wxt/chrome-data'), //.wxt/chrome-data 需要自己手动创建
+    keepProfileChanges: true,
+});
